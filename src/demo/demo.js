24dc439a9179ed06bbe8fb7ea9d9c54d3a9b7b4a
Yeh-Yung <yyc1217@gmail.com>
2019-11-07 13:30:49 +0800

    Update demo page.

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -2,14 +2,11 @@ import './demo.scss'
 
 import Vue from 'vue'
 import components, { bus, events } from '../'
-import insertSql from './insert-sql.vue'
 
 let event = events.changeDegree.withID('demo-degree-5')
 bus.$on(event, function (data) {
   document.getElementById('demoCallback').textContent = data.degree.id
 })
-
-components.insertSql = insertSql
 
 new Vue({
   el: '#app',
